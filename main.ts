3782eeeb406b728345577ea46866d42182094bfe
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2025-09-24 11:22:23 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,19 @@
+let Hand = 0
+input.onGesture(Gesture.Shake, function () {
+    Hand = randint(1, 3)
+    if (Hand == 1) {
+        basic.showIcon(IconNames.Scissors)
+    }
+    if (Hand == 2) {
+        basic.showIcon(IconNames.Chessboard)
+    }
+    if (Hand == 3) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
